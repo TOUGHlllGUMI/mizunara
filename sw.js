b5401061af85dbaa4f33db7c95370fefe0959688
user <kanzochida@gmail.com>
2026-08-11 17:19:30 +0900

    ロゴを「GN-キルゴシック(キルゴU)」フォントの画像ワードマークに変更
    フォント名を特定(readme.txt確認済み、作者:Getsuren/残雪、商用利用可)。
    ただしライセンスがフォントファイル自体のWeb再配布を控えるよう求めているため、
    .ttfは埋め込まずローカルでラスタライズしたPNGロゴのみをリポジトリに含める
    (readme内の「アウトラインデータ化しての改変は自由」の記載に沿った対応)。

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'mizunara-cache-v1';
+const CACHE_NAME = 'mizunara-cache-v3';
 const CORE_ASSETS = [
   './',
   './index.html',
@@ -7,7 +7,8 @@ const CORE_ASSETS = [
   './script.js',
   './manifest.json',
   './icons/icon-192.png',
-  './icons/icon-512.png'
+  './icons/icon-512.png',
+  './assets/logo.png'
 ];
 
 self.addEventListener('install', (event) => {
